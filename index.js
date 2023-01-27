5524f5d5d318dff5b843db96592e1ae6e71e98be
LeeVincentCampbell <leevcampbell@gmail.com>
2023-01-27 16:06:30 -0700

    added to index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+    return a + b
+}
+function subtract(a, b){
+    return a - b
+}
+function multiply(a, b){
+    return a * b
+}
+function divide(a, b){
+    return a / b
+}
+function increment(n){
+    let number = n
+    return ++number
+}
+function decrement(n){
+    return --n
+}
+function makeInt(n){
+   n = parseInt(n, 10)
+    return n
+}
+function preserveDecimal(n){
+    n = parseFloat(n)
+    return n
+}
